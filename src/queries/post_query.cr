283da977d4a1943b70f35137d6a3cf68562c7b15
mikeeus <itsmikias@gmail.com>
2018-07-02 12:48:34 +0300

    Create post query with scopes

--- src/queries/post_query.cr
+++ src/queries/post_query.cr
@@ -1,2 +1,26 @@
 class PostQuery < Post::BaseQuery
+  PER_PAGE = 5
+
+  def latest(page : Int = 1)
+    published_at.lte(Time.now)
+      .published_at.desc_order
+      .limit(PER_PAGE)
+      .offset(PER_PAGE * (page - 1))
+  end
+
+  def published
+    published_at.lte(Time.now).published_at.desc_order
+  end
+
+  def published_search(term : String)
+    if term
+      published.content.ilike("%#{term}%")
+    else
+      published
+    end
+  end
+
+  def find_published_by_slug(slug : String)
+    published.slug(slug).first
+  end
 end
