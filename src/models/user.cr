7c02df51d2bdb03b43e9bc9eff6ea6d96fa29a0e
mikeeus <itsmikias@gmail.com>
2018-07-08 11:14:47 +0300

    Include Authentic and Carbom modules

--- src/models/user.cr
+++ src/models/user.cr
@@ -1,7 +1,14 @@
 class User < BaseModel
+  include Carbon::Emailable
+  include Authentic::PasswordAuthenticatable
+
   table :users do
     column name : String
     column email : String
     column encrypted_password : String
+  end
+
+  def emailable
+    Carbon::Address.new(email)
   end
 end
