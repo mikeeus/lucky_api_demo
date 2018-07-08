523460667279441b164e5a517f63783f8baa86ad
mikeeus <itsmikias@gmail.com>
2018-07-08 12:20:33 +0300

    Protect with AuthenticatedAction

--- src/actions/posts/create.cr
+++ src/actions/posts/create.cr
@@ -1,6 +1,6 @@
-class Posts::Create < ApiAction
+class Posts::Create < AuthenticatedAction
   route do
-    post = PostForm.create!(params, author: UserQuery.new.first)
+    post = PostForm.create!(params, author: current_user)
     json Posts::ShowSerializer.new(post), Status::Created
   end
 end
